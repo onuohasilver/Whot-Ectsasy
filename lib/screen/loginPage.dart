ab2bddcb848c3972ec20c36645c563dcfe700158
onuohasilver <onuohasilver9@gmail.com>
2020-06-12 12:17:18 +0100

    textInput Field

--- lib/screen/loginPage.dart
+++ lib/screen/loginPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:whot/components/cardBuilder.dart';
 import 'package:whot/constants.dart';
 import 'package:whot/gameLogic/appProvider.dart';
 
@@ -15,11 +16,76 @@ class _LoginScreenState extends State<LoginScreen> {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        decoration:kBackgroundImage,
-        height:height,
-        width: width,
-        
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: kBackgroundImage,
+          height: height,
+          width: width,
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: width * .4,
+                height: height,
+                color: Colors.white.withOpacity(.4),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Stack(
+                      children: [
+                        CircleAvatar(
+                          minRadius: height * .15,
+                          backgroundColor: Colors.brown[800].withOpacity(.3),
+                        ),
+                        Positioned.fill(
+                          top: 14,
+                          bottom: 5,
+                          child: CircleAvatar(
+                            minRadius: height * .14,
+                            backgroundColor: Colors.white.withOpacity(.7),
+                            child: Icon(Icons.person, size: 60),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Silver KetchUp',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                  width: width * .6,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      buildWhotCenter(
+                          Colors.white, height * 6, width * 3, false),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Container(
+                          width: width * .4,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(40)),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                hintText: 'Enter Email',
+                                hintStyle: TextStyle(),
+                                fillColor: Colors.white.withOpacity(.4),
+                                filled: true,
+                                focusColor: Colors.blue,
+                                border: InputBorder.none),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ))
+            ],
+          ),
+        ),
       ),
     );
   }
